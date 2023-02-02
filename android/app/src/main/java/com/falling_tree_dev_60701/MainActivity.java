247921c98b9d29945ff1d7060d89e1dd77535871
Crowdbotics <engineering@crowdbotics.com>
2023-02-02 02:47:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/falling_tree_dev_60701/MainActivity.java
@@ -0,0 +1,15 @@
+package com.falling_tree_dev_60701;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "falling_tree_dev_60701";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-tree-dev-60701",
+  url: "https://falling-tree-dev-60701.botics.co",
+  api: "https://falling-tree-dev-60701.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
